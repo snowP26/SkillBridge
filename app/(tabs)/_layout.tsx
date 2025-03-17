97fd8481b978819cf3d97c5705560eea2c38ac97
snowP_ <jverceluz@gbox.adnu.edu.ph>
2025-03-17 20:39:58 +0800

    fix: bottom bar format fix

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,6 @@
+import { View, Text } from "react-native";
 import { Tabs } from "expo-router";
-import { House } from "lucide-react-native";
+import { House, MessagesSquare, BadgePlus, Heart, CircleUserRound} from "lucide-react-native";
 import '../globals.css';
 
 export default function TabsLayout() {
@@ -16,10 +17,69 @@ export default function TabsLayout() {
                 name="index"
                 options={{
                     tabBarLabel: "Home",
-                    tabBarIcon: ( {color} ) => <House color={ color }/> ,
-
+                    tabBarIcon: ( {color} ) => <House color={ color }/>,
                 }}
             />
+
+            <Tabs.Screen
+                name="inbox"
+                options={{
+                    tabBarLabel: "Inbox",
+                    tabBarIcon: ( {color} ) => <MessagesSquare color={ color }/>,
+                }}
+            />
+            
+            <Tabs.Screen
+                name="create"
+                options={{
+                    tabBarLabel: "",
+                    tabBarIcon: ( {focused} ) => (
+                        <View style={{
+                            borderBottomWidth: 10,
+                            borderBottomColor: "transparent",
+                        }}>
+                            <View
+                                style={{
+                                    width: 91,
+                                    height: 91,
+                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    backgroundColor: "#A6E1FA",
+                                    borderRadius: "100%",
+                                }}
+                            >
+                                <BadgePlus color={ focused ? "white" : "#0E6BA8" } size={66}/>
+                                <Text
+                                    style={{
+                                        fontSize: 12,
+                                        color: focused ? "white" : "#0E6BA8",
+                                        fontWeight: "bold",
+                                        
+                                    }}
+                                >Create</Text>
+                            </View>
+                    </View>
+                    ),
+                    
+                }}
+            />
+
+            <Tabs.Screen
+                name="favorites"
+                options={{
+                    tabBarLabel: "Favorites",
+                    tabBarIcon: ( {color} ) => <Heart color={ color }/>,
+                }}
+            />
+            
+            <Tabs.Screen
+                name="profile"
+                options={{
+                    tabBarLabel: "Profile",
+                    tabBarIcon: ( {color} ) => <CircleUserRound color={ color }/>,
+                }}
+            />
+
         </Tabs>
     )
 } 
