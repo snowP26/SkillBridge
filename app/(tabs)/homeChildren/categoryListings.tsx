0e73b82c7ae6359e78ec5b81dc04ed3cfbee15c9
James Gabriel Verceluz <james@Jamess-MacBook-Air.local>
2025-04-27 01:28:58 +0800

    feat: listing screen finish

--- app/(tabs)/homeChildren/categoryListings.tsx
+++ app/(tabs)/homeChildren/categoryListings.tsx
@@ -11,20 +11,34 @@ import {
     Text } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import CategoryBanner from "@/components/CategoryBanner";
+import DisplayListings from "@/components/DisplayListings";
+import { useRouter } from "expo-router";
 
 export default function categoryListings(){
+    const router = useRouter();
+
     return (
         <LinearGradient
             colors={ ["#00072D", "#00072D", "#00072D", "#00072D", "#0A2472", "#0A2472"] } 
             style={ styles.background }
         >
             <CategoryBanner category="Keyboard" />
-            <Text className="text-[#B9D7FF] font-extrabold text-[40px] self-center mt-6">
+            <Text className="text-[#B9D7FF] font-extrabold text-[40px] self-center my-3">
                 Custom Keyboards
             </Text>
-            <Text>
+            <Text className="text-white my-1 ml-4">
                 Sorted by: Recent Listings
             </Text>
+            <DisplayListings
+                jobTitle="I need someone to mod my keyboard"
+                Location="Naga City"
+                dueDate="26 April 2025"
+                Author="Robert Chase"
+                time="12 hours"
+                onPress={() => router.push("/(tabs)/homeChildren/keybCat")}
+                isNego={false}
+                Price={500}
+            />
         </LinearGradient>
     );
 
--- components/DisplayListings.tsx
+++ components/DisplayListings.tsx
@@ -1,6 +1,7 @@
 import { View, Text, TouchableOpacity } from 'react-native'
 import { CircleUserRound, Heart } from "lucide-react-native";
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 type Props = {
     jobTitle: string;
@@ -10,29 +11,38 @@ type Props = {
     time: string;
     onPress?: any;
     isNego: boolean;
+    Price: number;
 }
 
-export default function DisplayFavorites({ jobTitle, Location, dueDate, Author, time, onPress, isNego }: Props){
+export default function DisplayListings({ jobTitle, Location, dueDate, Author, time, onPress, isNego, Price }: Props){
     return (
         <TouchableOpacity onPress={onPress}>
             <View className="h-[1px] bg-white w-[90%] self-center my-1.5" />
-            <CircleUserRound color="white" size="35"/>
-            <View className="flex-row mt-1">
-                <View className="bg-[#2C3E50] px-4 py-2 rounded-full mr-2">
-                    <Text className="text-white text-sm">{ Location }</Text>
+                <View className='max-w-[360px] ml-5 my-3'>
+                    <Text className='text-white font-bold text-xl mb-2'>{ jobTitle }</Text>
+                    {/* Start of bottom part */}
+                    <View className="flex flex-row">
+                        <CircleUserRound color="white" size="70"/>
+                        <View className="flex-col ml-2">
+                            <View className="flex-row mt-1">
+                                <View className="bg-[#2C3E50] px-3 py-2 rounded-full mr-1">
+                                    <Text className="text-white text-sm">{ Location }</Text>
+                                </View>
+                                <View className="bg-[#2C3E50] px-3 py-2 rounded-full">
+                                    <Text className="text-white text-sm">{ dueDate }</Text>
+                                </View>
+                            </View>
+                            <Text className='text-white mt-4 text-xs'>Posted by { Author } { time } ago</Text>                        
+                        </View>
+                        <View className='bg-[#001C55] rounded-full h-[50px] w-[95px] flex-col items-center justify-center'>
+                            <Text className='text-[#A6E1FA] font-bold text-xl'>PHP { Price }</Text>
+                            {isNego ? (<Text className='text-white text-[8px]'>Negotiable</Text>) : (<Text className='text-white text-xs'>Fixed</Text>)}
+                        </View>
+                    </View>                
                 </View>
-                <View className="bg-[#2C3E50] px-4 py-2 rounded-full">
-                    <Text className="text-white text-sm">{ dueDate }</Text>
-                </View>
-                <View className="bg-[#0A2472] px-4 py-2 rounded-full">
-                    <Text className="text-[#A6E1FA] text-[18px] font-bold">{ dueDate }</Text>
-                    if(isNego){
-                        <Text className="text-[#A6E1FA] text-[8px]">Negotiable</Text>
-                    } else {
-                        <Text className="text-[#A6E1FA] text-[8px]">Fixed</Text>
-                    }
-                </View>
-            </View>
+
+
+
             <View className="h-[1px] bg-white w-[90%] self-center my-1.5" />
         </TouchableOpacity>
     );
